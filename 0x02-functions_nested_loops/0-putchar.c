7d9d1d12e7d57f1a5f267b9ddbc847d58aa18130
beleir101 <beleirmessay2436@gmail.com>
2022-12-13 09:17:55 -0800

    put trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,27 @@
 #include "main.h"
+#include <unistd.h>
+
+
+
 /**
- * main - Entry point
  *
- * Return: nothing
+ *  _putchar - writes the character c to stdo
+ *
+ *  
+ *
+ * Return: On success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ *
  */
+
+int _putchar(char c)
+
+{
+
+		return (write(1, &c, 1));
+
+}
 
 int main(void)
 {
