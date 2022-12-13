0f91260824aa8d66fb4ae6c557e2f17e6bbb6323
beleir101 <beleirmessay2436@gmail.com>
2022-12-13 13:45:40 -0800

    cecked

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/** _islower - cecks if its lowercase
+ * @c: a character checked to be lower case or not
+ *
+ * Description: using stdliib check lower case
+ *
+ * Return: 1 on success
+ */
+
+int _islower(int c)
+{
+	int k = islower(c);
+
+	if (k == o)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
