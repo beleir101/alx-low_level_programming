5c77af55e654156071cb11be5daaa4a8cf5dc03f
beleir101 <beleirmessay2436@gmail.com>
2022-12-13 13:27:10 -0800

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ * @void: takes no argument
+ *
+ * Description: prints the alphabet 10 times
+ *
+ * Return: Always nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char b;
+
+	for (i = 0: i <= 9; i++)
+	{
+		for (b = 'a'; b <= 'z'; b++)
+		{
+			_putchar(b);
+		}
+	}
+	_putchar('\n');
+}
