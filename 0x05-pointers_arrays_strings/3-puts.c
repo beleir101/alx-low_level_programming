33ab2fb854ee5fb024a55545a28a4c98e4014de0
beleir101 <beleir101@DESKTOP-AVU962C.localdomain>
2022-12-19 14:36:51 +0300

    point to my sucess

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - main func
+ * @s: string to pe displayed
+ *
+ * Description: This function will display a string
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *s)
+{
+	int thing = 0;
+
+	while (s[thing] != '\0')
+		_putchar(s[thing]);
+	_putchar('\n');
+}
