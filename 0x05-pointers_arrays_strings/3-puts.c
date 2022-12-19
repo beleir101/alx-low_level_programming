4db77203b62b49824f3d5e59600921cbcf86f92c
beleir101 <beleir101@DESKTOP-AVU962C.localdomain>
2022-12-19 14:41:28 +0300

    point to my sucess

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,20 +2,20 @@
 
 /**
  * _puts - main func
- * @s: string to pe displayed
+ * @str: string to pe displayed
  *
  * Description: This function will display a string
  *
  * Return: Nothing
  */
 
-void _puts(char *s)
+void _puts(char *str)
 {
 	int thing = 0;
 
-	while (s[thing] != '\0')
+	while (str[thing] != '\0')
 	{
-		_putchar(s[thing]);
+		_putchar(str[thing]);
 		thing++;
 	}
 	_putchar('\n');
