4d3a8bc77532ebe77299694de4adc3da7c54ce03
beleir101 <beleir101@DESKTOP-AVU962C.localdomain>
2022-12-19 16:51:21 +0300

    point to my sucess

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - main func
+ * @str: arg 1
+ *
+ * Description: will print the other half of the string
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int p = 0;
+	int k;
+
+	while (str[p] != '\0')
+	{
+		p++;
+	}
+	if (p % 2 == 0)
+	{
+		k = p / 2;
+		for (; k < p; k++)
+		{
+			_putchar(str[k]);
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		k = (p - 1) / 2;
+		for (; k < p; k++)
+		{
+			_putchar(str[k]);
+		}
+		_putchar('\n');
+	}
+}
