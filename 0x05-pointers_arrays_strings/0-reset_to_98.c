1d088620b886a23bb8ce74da8a2a0d3bf332c30d
beleir101 <beleir101@DESKTOP-AVU962C.localdomain>
2022-12-19 13:37:41 +0300

    point to my sucess

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** 
+ * reset_to_98 - check func
+ * @*n: argument that accepts a poiner
+ *
+ * Description: This function will reset everything to 98
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is to learn pointers in C programming
