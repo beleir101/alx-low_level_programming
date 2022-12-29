ff23569339830438fc9d2298664a59d494c3cdb1
beleir101 <beleirmessay2436@gmail.com>
2022-12-29 16:24:20 +0300

    gave up

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * cap_string - main func
+ * @: string to be converted
+ *
+ * Description: This will change every character of a string to caps
+ * Return: a pointer
+ */
+
+char *cap_string(char *)
+{
+	int i = 0;
+	
