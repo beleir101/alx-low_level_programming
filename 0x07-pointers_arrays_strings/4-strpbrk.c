f5e499e167419e0b88dd82a7f4442a10d33aebcc
beleir101 <beleirmessay2436@gmail.com>
2023-01-02 20:48:39 +0300

    pointer

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - main func
+ * @s: first argument
+ * @accept: second argument
+ * Description: will search for a string
+ * Return: either pointer or null
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *start = accept;
+
+	while (*s)
+	{
+		while (*accept)
+		{
+			if (*accept == *s)
+				return (s);
+			accept++;
+		}
+
+		accept = start;
+		s++;
+	}
+	return (NULL);
+}
