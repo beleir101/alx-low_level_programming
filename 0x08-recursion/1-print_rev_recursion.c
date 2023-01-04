9870a03dc3c7680429b7c9f12054e2b263a7adc3
beleir101 <beleirmessay2436@gmail.com>
2023-01-04 21:43:50 +0300

    recur

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - main func
+ * @s: string to be reversed
+ * Description: this will reverse a string
+ *
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
