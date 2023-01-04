746307c98230c52da64a22c8a626f862e149c633
beleir101 <beleirmessay2436@gmail.com>
2023-01-04 21:57:45 +0300

    recur

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -15,8 +15,8 @@ int _strlen_recursion(char *s)
 	if (*s != 0)
 	{
 		i++;
-		return(i + _strlen_recursion(s + 1));
+		return (i + _strlen_recursion(s + 1));
 	}
 	else
-		return(0);
+		return (0);
 }
