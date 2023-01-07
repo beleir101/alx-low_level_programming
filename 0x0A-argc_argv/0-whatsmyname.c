cad0ebb379b6e6d8d26e184403523377955d367b
beleir101 <beleirmessay2436@gmail.com>
2023-01-07 15:02:37 +0300

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main func
+ * @argc: number of arguments
+ * @argv: array of argumentds
+ *
+ * Description: Prints out name of file
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
