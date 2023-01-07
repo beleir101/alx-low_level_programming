8811ad4360fa3e74fbd5ffb6918c40f48bb3c74e
beleir101 <beleirmessay2436@gmail.com>
2023-01-07 15:11:54 +0300

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - main func
+ * @argc: number of arguments
+ * @argv: values of arguments
+ *
+ * Description: described
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc);
+	}
+
+	return (0);
+}
