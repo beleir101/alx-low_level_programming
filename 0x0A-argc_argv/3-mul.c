24e367fee1bcb5d7fb35c8f9e3ec700012d18c5d
beleir101 <beleirmessay2436@gmail.com>
2023-01-08 11:53:21 +0300

    argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mainfunc
+ * @argc: int
+ * @argv: array
+ *
+ * Description: described
+ * Return: 1 or 0 depending
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		int rsult, i = atoi(argv[1]), j = atoi(argv[2]);
+		rsult = i * j;
+		printf("%d\n", rsult);
+		return (0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+}
