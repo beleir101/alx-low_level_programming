5a179da074d6a8202116d68ea212a9adc0ba3e0c
beleir101 <beleirmessay2436@gmail.com>
2023-01-17 21:06:49 +0300

    pre

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+practicing structs and typedef in C
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - short dedscription
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: secribed
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
