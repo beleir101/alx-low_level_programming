839cf6cd0997080af38db43f7627f0a588ea1926
beleir101 <beleirmessay2436@gmail.com>
2023-01-17 18:55:00 +0300

    pre

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main func
+ * Description: described
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
