a5bf9b617d57c4544c21294eb247997ff8f152af
beleir101 <beleirmessay2436@gmail.com>
2023-01-26 22:31:30 +0300

    linked

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef ART
+#define ART
+#include <stdlib.h>
+
+/**
+ * struct list_s - structure
+ * @str: string
+ * @len: length
+ * @next: next node
+ *
+ * Description: Described
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct lists_ *next;
+} list_t;
+
+size_t print_list(const list_t *h);
